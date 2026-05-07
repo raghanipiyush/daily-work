23e7bc93011cc9e674f3e583c083e0dc3e531210
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-07 16:49:04 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("1. hello world");
+	printf("\n2. hello world"); 
+	printf("\n3. hello world");
+	printf("\n4. hello world");
+	printf("\n \t5. hello world");
+}

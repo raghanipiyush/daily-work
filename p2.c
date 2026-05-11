78f8b1643be2f157aceb2161963e74780e205b77
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-11 17:29:32 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 100;
+	float b;
+	
+	b = 22.12;
+	printf("value of b is: %f and a is: %d", b,a);
+	
+}
+/*output
+value of b is: 22.120001 and a is: 100
+*/

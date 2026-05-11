6b711e1fb7074bb779a4136d996b355c6a9f1621
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-11 17:49:53 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// #define a 1111
+
+int main()
+{
+	int a, b;
+	printf("Enter a :");
+	scanf("%d" , &a);
+	printf("Enter b :");
+	scanf("%d" , &b);
+	
+	printf("addition is: %d", a + b);
+	
+}
+/*output
+Enter a :12
+Enter b :23
+addition is: 35
+
+*/

9e242eb21575a5ce75f0a757b757c4680410de9f
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-14 17:17:44 +0530

    Add files via upload

---
+++ p18.c
@@ -0,0 +1,12 @@
+//ternory operator => condition ? true : false
+
+#include<stdio.h>
+int main()
+{
+	int a = 101,b = 22;
+	
+	a > b ? printf("a is max"): printf("b is max");
+}
+/*output
+a is max
+*/

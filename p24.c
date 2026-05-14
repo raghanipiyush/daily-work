dfac584b8317cdeb5265f068d7d4f81edbec7375
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-14 17:19:59 +0530

    Add files via upload

---
+++ p24.c
@@ -0,0 +1,20 @@
+//ladder
+#include<stdio.h>
+int main()
+{
+	int marks = 84;
+	if(marks >=85){
+		printf("grade A");
+	}else if(marks >=70){
+		printf("grade B");
+	}else if(marks >=50){
+		printf("grade c");
+	}else if(marks >=35){
+		printf("grade D");
+	}else{
+		printf("better luck next time bro!!!");
+	}
+		
+
+}
+

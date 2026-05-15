b8c01047f8c42278ff0b3961b8c45383ea54e41e
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-15 17:40:14 +0530

    Add files via upload

---
+++ p36.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a = 1, b = 1, c, i;
+	printf("%d %d ", a, b);
+	
+	for(i = 1; i <= 4; i++){
+		c = a + b;
+		printf("%d ", c);
+		a = b;
+		b = c;
+	}
+}
+/*output
+1 1 2 3 5 8
+*/

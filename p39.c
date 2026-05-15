24032be9cbc22e35987667826d95b32cc8e5911b
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-15 17:40:53 +0530

    Add files via upload

---
+++ p39.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n = 5, i, sum = 0;
+	for(i = 1; i <= n; i++){
+		printf("%d ", i);
+		sum = sum = i;
+	}
+	printf("\n sum = %d", sum);
+			
+}
+/*output
+1 2 3 4 5
+ sum = 5
+*/

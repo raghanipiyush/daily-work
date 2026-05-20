96e2559c1a04a9f52e6f55a90534d401dde65379
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-20 16:58:00 +0530

    Add files via upload

---
+++ p52.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a[5] = {7, 23, 45, 56, 59};
+	
+	
+	printf("%d ", a[0]);
+	printf("%d ", a[1]);
+	printf("%d ", a[2]);
+	printf("%d ", a[3]);
+    printf("%d ", a[4]);
+}

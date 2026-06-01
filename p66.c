3a5a023f71d0447448b3de21775ef1a9eb15a5a0
raghanipiyush <raghanipiyush1@gmail.com>
2026-06-01 17:17:36 +0530

    Add files via upload

---
+++ p66.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a[5];
+	printf("size of int: %d ", sizeof(a));
+	printf("\nsize of float: %d ", sizeof(float));
+	printf("\nsize of char: %d ", sizeof(char));
+	
+}
+/*output
+size of int: 20
+size of float: 4
+size of char: 1
+*/
